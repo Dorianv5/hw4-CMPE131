fc9e88027ea6993385f17988394bf23749abe169
Dorianv5 <118552196+Dorianv5@users.noreply.github.com>
2023-03-20 23:58:07 -0700

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,6 +1,6 @@
 palindrome.py
 def palindrome(list):
-    for i in len(list/2)
-        if list[i] != list[len(list) - i]
+    for i in len(list/2):
+        if list[i] != list[len(list) - i]:
             return false
     return true
