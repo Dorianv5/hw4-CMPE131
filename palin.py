3302307eacbf723964e07537e28052f2aa555c0b
Dorianv5 <118552196+Dorianv5@users.noreply.github.com>
2023-03-21 00:01:12 -0700

    Update palin.py

--- palin.py
+++ palin.py
@@ -2,5 +2,5 @@
 def palindrome(list):
     for i in range(len(list) // 2):
         if list[i] != list[len(list) - i - 1]:
-            return false
-    return true
+            return False
+    return True
